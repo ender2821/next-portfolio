96544419080feb5e88e7655161c6dc3aa4180117
Joshua Jensen <ender2821@yahoo.com>
2025-02-18 12:35:49 -0700

    🚧 wip: added sanity live fetch API

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import ImageSlider from "@/components/ImageSlider";
 import Image from "next/image";
 import Link from "next/link";
+import { sanityFetch } from "@/sanity/lib/live";
+import { HOME_QUERY } from "@/queries/homeQueries";
 
-export default function Home() {
+export default async function Home() {
+  const { data: home } = await sanityFetch({ query: HOME_QUERY, params: {} });
+  console.log(home);
   return (
     <main className="flex min-h-screen flex-col items-center pt-24 pb-24">
       <ImageSlider />
