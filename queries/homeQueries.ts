bed9f6b2f9d023d65017c010c8ca3c2ba5464565
Joshua Jensen <ender2821@yahoo.com>
2025-02-18 10:12:36 -0700

    🚧 wip: added service queries

--- queries/homeQueries.ts
+++ queries/homeQueries.ts
@@ -1,36 +1,39 @@
 import { defineQuery } from "next-sanity";
 
 export const HOME_QUERY = defineQuery(`
-*[_type == "home"][0]{
-  heroTitle,
-  heroImage->{
-    "imageUrl": image.asset->url,
-    "imageName": name,
-  },
-  heroCTAButtonTxt,
-  workGallery[]->{
-    name,
-    slug,
-    workPageLayout[0]{
-      workLayoutGallery[0]->{
-        "imageUrl": image.asset->url,
-        "imageName": name,
-      }
+  *[_type == "home"][0]{
+    heroTitle,
+    heroImage->{
+      "imageUrl": image.asset->url,
+      "imageName": name,
     },
-  },
-  workCTAButtonTxt,
-  serviceCTAButtonTxt,
-  serviceHomeCategories,
-  aboutSubtitle,
-  aboutContent,
-  aboutGithub,
-  aboutLinkedIn,
-  meetGlacierSubtitle,
-  meetGlacierContent,
-  meetGlacierButtonCTA,
-  glacierGallery->{
-    "imageUrl": image.asset->url,
-    "imageName": name,
-  },
-}
+    heroCTAButtonTxt,
+    workGallery[]->{
+      name,
+      slug,
+      workPageLayout[0]{
+        workLayoutGallery[0]->{
+          "imageUrl": image.asset->url,
+          "imageName": name,
+        }
+      },
+    },
+    workCTAButtonTxt,
+    serviceCTAButtonTxt,
+    serviceHomeCategories[]->{
+      serviceCatergoryTitle,
+      serviceCategoryDescription
+    },
+    aboutSubtitle,
+    aboutContent,
+    aboutGithub,
+    aboutLinkedIn,
+    meetGlacierSubtitle,
+    meetGlacierContent,
+    meetGlacierButtonCTA,
+    glacierGallery->{
+      "imageUrl": image.asset->url,
+      "imageName": name,
+    },
+  }
 `);
---
+++ queries/servicesQueries.ts
@@ -0,0 +1,24 @@
+import { defineQuery } from "next-sanity";
+
+export const SERVICES_QUERY = defineQuery(`
+  *[_type == "services"][0]{
+    servicesHeroSubtitle,
+    servicesHeroImage->{
+      "imageUrl": image.asset->url,
+      "imageName": name,
+    },
+    servicesCtaButtonTxt,
+    servicesServiceList[]->{
+      serviceCatergoryTitle,
+      serviceCategoryImage->{
+        "imageUrl": image.asset->url,
+        "imageName": name,
+      },
+      serviceCategories[]{
+        serviceCatergoryListTitle,
+        serviceCatergoryListTags,
+        serviceCatergoryListDescriptions
+      }
+    }
+  }
+`);
