8960bb8bda9b7586ab5529965da0a3cb6f74cef3
Joshua Jensen <ender2821@yahoo.com>
2025-03-03 20:50:04 -0700

    🚧 wip: fix z-index issue

--- components/HomeGlacier.tsx
+++ components/HomeGlacier.tsx
@@ -57,7 +57,7 @@ export default function HomeGlacier(props: HomeGlacierProps) {
           toggleDrawer={toggleDrawer}
           isOpen={isOpen}
           className={
-            "col-span-6 sm:col-start-4 sm:row-start-2 sm:row-end-4 sm:col-span-3 relative lg:bottom-[-2rem] lg:col-span-2 lg:col-start-5 lg:row-start-1 lg:row-span-2"
+            "z-10 col-span-6 sm:col-start-4 sm:row-start-2 sm:row-end-4 sm:col-span-3 relative lg:bottom-[-2rem] lg:col-span-2 lg:col-start-5 lg:row-start-1 lg:row-span-2"
           }
           shadow={"left"}
         />
