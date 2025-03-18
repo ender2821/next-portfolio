dafca45273d900eb2e1381c8ad73b1378e1ad0c9
Joshua Jensen <ender2821@yahoo.com>
2025-03-17 23:22:18 -0600

    🚧 wip: add hover states to home services

--- components/HomeServices.tsx
+++ components/HomeServices.tsx
@@ -18,17 +18,24 @@ export default function HomeServices({ categories }: HomeServicesProps) {
     <>
       <div className="hidden lg:block col-span-6">
         {categories.map((category: ServiceCategory, i) => (
-          <div key={i} className="group ">
-            <h3
-              className={`${raleway.className} font-bold text-black-decoration text-right uppercase text-[5rem] group-hover:text-[3.85rem] transition-all`}
-            >
-              {category.serviceCatergoryTitle}
-            </h3>
-            {category?.serviceCategoryDescription && (
-              <div>
-                <PortableText value={category.serviceCategoryDescription} />
-              </div>
-            )}
+          <div
+            key={i}
+            className="group overflow-hidden border-black-decoration border-b pb-8 pt-8"
+          >
+            <div className="flex-nowrap gap-4 group-hover:-left-1/2 left-0 flex transition-all relative items-end w-[150%]">
+              <h3
+                className={`${raleway.className} mb-0 leading-[4rem] group-hover:leading-[3rem] block flex-[100%] font-bold text-black-decoration text-right uppercase text-[5rem] group-hover:text-[3.85rem] group-hover:left-0 transition-all`}
+              >
+                {category.serviceCatergoryTitle}
+              </h3>
+              {category?.serviceCategoryDescription && (
+                <div className="flex-[50%] -mb-4">
+                  <div className="max-w-[900px]">
+                    <PortableText value={category.serviceCategoryDescription} />
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
         ))}
       </div>
