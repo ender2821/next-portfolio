8c9967c813bd7917a7f815955e89feb2e773fba6
Joshua Jensen <ender2821@yahoo.com>
2025-03-17 23:46:41 -0600

    🚧 wip: add border states to home services

--- components/HomeServices.tsx
+++ components/HomeServices.tsx
@@ -20,17 +20,18 @@ export default function HomeServices({ categories }: HomeServicesProps) {
         {categories.map((category: ServiceCategory, i) => (
           <div
             key={i}
-            className="group overflow-hidden border-black-decoration border-b pb-8 pt-8"
+            className="group overflow-hidden border-black-decoration border-b pb-8 pt-8 relative"
           >
-            <div className="flex-nowrap gap-4 group-hover:-left-1/2 left-0 flex transition-all relative items-end w-[150%]">
+            <span className="h-[1px] border-t z-10 border-blue border-dashed block absolute top-1/2 w-[calc(50%+1rem)] -left-[calc(50%+1rem)] group-hover:left-0 duration-500 transition-all"></span>
+            <div className="flex-nowrap gap-4 group-hover:-left-1/2 left-0 flex transition-all duration-500 relative items-end w-[calc(150%+1rem)]">
               <h3
-                className={`${raleway.className} mb-0 leading-[4rem] group-hover:leading-[3rem] block flex-[100%] font-bold text-black-decoration text-right uppercase text-[5rem] group-hover:text-[3.85rem] group-hover:left-0 transition-all`}
+                className={`${raleway.className} mb-0 leading-[4rem] group-hover:leading-[3rem] block flex-[100%] font-bold text-black-decoration text-right uppercase text-[5rem] group-hover:text-[3.85rem] group-hover:left-0 duration-500 transition-all`}
               >
                 {category.serviceCatergoryTitle}
               </h3>
               {category?.serviceCategoryDescription && (
                 <div className="flex-[50%] -mb-4">
-                  <div className="max-w-[900px]">
+                  <div className="max-w-[900px] mr-8 border border-dashed border-blue ">
                     <PortableText value={category.serviceCategoryDescription} />
                   </div>
                 </div>
