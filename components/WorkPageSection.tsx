73494b0b142c7cbd465075c46b00312db257038e
Joshua Jensen <ender2821@yahoo.com>
2025-03-31 17:47:22 -0600

    🚧 wip: add basic styles to work page sections

--- components/WorkPageSection.tsx
+++ components/WorkPageSection.tsx
@@ -22,17 +22,17 @@ export default function WorkPageSection(props: WorkPageSectionProps) {
       {i === 0 && (
         <div className="sectionMtnRightWhite absolute right-0 top-0 -translate-y-1/2 z-30" />
       )}
+      {workLayoutTitle ? (
+        <h2
+          className={`${isEven(i) ? "text-right" : "text-left"} text-[3rem] text-black-bg leading-[2.5rem] lg:text-[3.75rem] lg:leading-[3rem] mt-4 sm:mt-0 lg:mb-8 col-span-6 md:col-span-4 md:col-start-2`}
+        >
+          {workLayoutTitle}
+        </h2>
+      ) : null}
       <div
-        className={`col-span-6 sm:pl-4 sm:pt-16 xl:pt-[10rem] sm:col-span-3 z-10`}
+        className={`${isEven(i) ? "sm:col-start-4" : "sm:col-start-2"} sm:pl-4 sm:col-span-2 z-10`}
       >
-        {workLayoutTitle ? (
-          <h2
-            className={`text-[3rem] text-black-bg leading-[2.5rem] lg:text-[3.75rem] lg:leading-[3rem] mt-4 sm:mt-0 lg:mb-8`}
-          >
-            {workLayoutTitle}
-          </h2>
-        ) : null}
-        <div className="grid lg:grid-cols-2 xl:grid-cols-3 gap-4 text-black-bg">
+        <div className="grid lg:grid-cols-2 xl:grid-cols-2 gap-4 text-black-bg">
           <PortableText
             value={workLayoutContent ? workLayoutContent : []}
             components={{
@@ -40,7 +40,7 @@ export default function WorkPageSection(props: WorkPageSectionProps) {
                 normal: ({ children }) => {
                   return (
                     <p
-                      className={`${isEven(i) ? "" : workLayoutContent && workLayoutContent.length < 3 && "xl:first-of-type:col-start-2"} ${isEven(i) ? "" : workLayoutContent && workLayoutContent.length === 1 && "xl:first-of-type:col-start-3"} last-of-type:mb-0`}
+                      className={`${isEven(i) ? "" : workLayoutContent && workLayoutContent.length < 3 && "xl:first-of-type:col-start-1"} ${isEven(i) ? "" : workLayoutContent && workLayoutContent.length === 1 && "xl:first-of-type:col-start-3"} last-of-type:mb-0`}
                     >
                       {children}
                     </p>
@@ -52,15 +52,15 @@ export default function WorkPageSection(props: WorkPageSectionProps) {
         </div>
       </div>
       <div
-        className={`lg:flex items-end ${isEven(i) ? "lg:col-start-6" : "lg:col-start-1"} row-start-1`}
+        className={`lg:flex items-end ${isEven(i) ? "lg:col-start-2" : "lg:col-start-4"} md:col-span-2 row-start-2`}
       >
         <div className={``}>
           {images?.map((image: any, j: number) => (
             <Image
               src={image.imageUrl ?? "/default-image.jpg"}
               alt={image.imageName ?? "default image description"}
-              width={300}
-              height={300}
+              width={900}
+              height={1200}
               key={j}
             />
           ))}
