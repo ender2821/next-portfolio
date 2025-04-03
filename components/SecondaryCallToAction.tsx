fafc8d48920f40941e06978fa383a25f51c0b6e5
Joshua Jensen <ender2821@yahoo.com>
2025-04-02 18:04:45 -0600

    🚧 wip: add responsive styles to secondary cta button

--- components/SecondaryCallToAction.tsx
+++ components/SecondaryCallToAction.tsx
@@ -1,10 +1,19 @@
+"use client";
+
 import SiteButton from "./SiteButton"; // Replace with the correct path to SiteButton
+import { useTheme, useMediaQuery } from "@mui/material";
 
 export default function SecondaryCallToAction() {
+  const theme = useTheme();
+  const lg = useMediaQuery(theme.breakpoints.up("lg"));
   return (
-    <div className="fixed bottom-7 w-full flex justify-end pr-8 z-40">
-      <div className="relative xl:before:-left-[80vw] before:content-[''] before:absolute before:top-1/2 before:-translate-y-1/2 xl:before:h-[1px] xl:before:w-[80vw] xl:before:border xl:before:border-dashed xl:before:border-t xl:before:border-blue before:z-20">
-        <SiteButton href="/contact" selected={true}>
+    <div className="lg:fixed bottom-7 w-full flex justify-center lg:justify-end py-8 lg:py-0 pr-8 z-40">
+      <div className="relative lg:before:-left-[90vw] before:content-[''] before:absolute before:top-1/2 before:-translate-y-1/2 xl:before:h-[1px] lg:before:w-[90vw] lg:before:border lg:before:border-dashed lg:before:border-t lg:before:border-blue before:z-20">
+        <SiteButton
+          href="/contact"
+          selected={true}
+          textHover={lg ? "dark" : "light"}
+        >
           Like What you See? Let's chat
         </SiteButton>
       </div>
