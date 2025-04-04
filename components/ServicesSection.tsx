a63835b7a0977db9627803134f6fde9a38e078ef
Joshua Jensen <ender2821@yahoo.com>
2025-04-04 17:44:22 -0600

    🚧 wip: finish adding page data to services

--- components/ServicesSection.tsx
+++ components/ServicesSection.tsx
@@ -5,6 +5,8 @@ import {
   Slug,
 } from "@/__sanity-generated__/types";
 import SiteButton from "@/components/SiteButton";
+import { serviceCategoryList } from "@/sanity/schemas/serviceCategoryList";
+import { PortableText } from "@portabletext/react";
 
 interface ServicesSectionProps extends ServiceCategory {
   image: { imageUrl: string | null; imageName: string | null };
@@ -17,17 +19,35 @@ interface ServicesSectionProps extends ServiceCategory {
 }
 
 export default function ServicesSection(props: ServicesSectionProps) {
-  const { serviceCatergoryTitle, image } = props;
+  const { serviceCatergoryTitle, image, categories } = props;
   return (
     <section className="services-section">
-      <Image
-        src={image?.imageUrl ?? ""}
-        alt={image?.imageName ?? ""}
-        width={500}
-        height={500}
-        className="object-cover w-full h-full"
-      />
-      {serviceCatergoryTitle && <h2>{serviceCatergoryTitle}</h2>}
+      <div className="w-[300px] h-[300px] relative mb-8">
+        <Image
+          src={image?.imageUrl ?? ""}
+          alt={image?.imageName ?? ""}
+          width={500}
+          height={500}
+          className="object-cover w-full h-full"
+        />
+      </div>
+      <h2 className="text-3xl font-bold">{serviceCatergoryTitle}</h2>
+      {categories &&
+        categories.map((category, i) => (
+          <div key={i}>
+            <h3>{category?.serviceCatergoryListTitle}</h3>
+            <ul>
+              {category?.serviceCatergoryListTags?.map((tag, j) => (
+                <li key={j}>{tag}</li>
+              ))}
+            </ul>
+            <ul>
+              {category?.serviceCatergoryListDescriptions?.map((desc, k) => (
+                <li key={k}>{desc}</li>
+              ))}
+            </ul>
+          </div>
+        ))}
       <SiteButton href={`/work/?id=${props.id}`} className="service-link">
         View Work
       </SiteButton>
