d1703043b647152f4863c005372eaf7552232400
Joshua Jensen <ender2821@yahoo.com>
2025-05-05 18:06:35 -0600

    🚧 wip: add remaining form fields

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -5,9 +5,12 @@ import { ErrorMessage } from "@hookform/error-message";
 import Input from "@mui/material/Input";
 import Checkbox from "@mui/material/Checkbox";
 import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+
 import FormControlLabel from "@mui/material/FormControlLabel";
 import { sendEmail } from "@/utils";
 import { useState } from "react";
+import MenuItem from "@mui/material/MenuItem";
 
 export type FormData = {
   name: string;
@@ -16,7 +19,30 @@ export type FormData = {
   phone: string;
   projectInMind: boolean;
   subject: string;
+  message: string;
+  projectType: string;
+  estimatedBudget: string;
+  projectDescription: string;
 };
+
+const projectType = [
+  { value: "projectType", label: "Project Type" },
+  { value: "webSite", label: "Web Site" },
+  { value: "webApp", label: "Web App" },
+  { value: "logo", label: "Logo" },
+  { value: "illustration", label: "Illustration" },
+  { value: "branding", label: "Branding" },
+  { value: "print", label: "Print Media" },
+  { value: "other", label: "Other" },
+];
+
+const estimatedBudget = [
+  { value: "estimatedBudget", label: "Estimated Budget" },
+  { value: "lessThan1000", label: "Less than $1000" },
+  { value: "1000to5000", label: "$1000 - $5000" },
+  { value: "5000to10000", label: "$5000 - $10,000" },
+  { value: "moreThan10000", label: "More than $10,000" },
+];
 
 export default function ContactForm() {
   const [projectInMind, setProjectInMind] = useState(false); // Add state for checkbox
@@ -29,11 +55,10 @@ export default function ContactForm() {
   } = useForm<FormData>();
   const onSubmit: SubmitHandler<FormData> = (data) => sendEmail(data);
 
-  console.log(watch("name")); // watch input value by passing the name of it
-  console.log(errors);
+  console.log(watch("projectType")); // watch input value by passing the name of it
   return (
     /* "handleSubmit" will validate your inputs before invoking "onSubmit" */
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
       {/* register your input into the hook by invoking the "register" function */}
       <Input
         placeholder="name"
@@ -71,9 +96,42 @@ export default function ContactForm() {
         }}
       />
       {projectInMind ? (
-        <Input placeholder="project" {...register("project")} />
+        <>
+          <TextField
+            id="project-type"
+            select
+            defaultValue="projectType"
+            {...register("projectType")}
+          >
+            {projectType.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField
+            id="estimated-budget"
+            select
+            defaultValue="estimatedBudget"
+            {...register("estimatedBudget")}
+          >
+            {estimatedBudget.map((option) => (
+              <MenuItem key={option.value} value={option.value}>
+                {option.label}
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField
+            {...register("projectDescription")}
+            multiline
+            placeholder="Project Description"
+          />
+        </>
       ) : (
-        <Input placeholder="subject" {...register("subject")} />
+        <>
+          <Input placeholder="subject" {...register("subject")} />
+          <TextField {...register("message")} multiline placeholder="Message" />
+        </>
       )}
       <Button type="submit">Submit</Button>
     </form>
